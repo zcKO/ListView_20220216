bc891ad63d7e7004562e73325fd1ae45d8dfcdaa
JaeCheon <studyJcHub@gmail.com>
2022-02-16 15:00:24 +0900

    학생 한명 보여줄 클래스 추가

---
+++ app/src/main/java/com/jc/listview_20220216/datas/StudentData.kt
@@ -0,0 +1,10 @@
+package com.jc.listview_20220216.datas
+
+// 나이는 잘 지정하지 않는다. 나이는 변하기 때문이다.
+// 그렇기에 생년 월일을 적게 한다. -> 생년 월일은 변하지 않는다.
+class StudentData(
+    val name: String,
+    val birthYear: Int,
+    val address: String
+) {
+}
