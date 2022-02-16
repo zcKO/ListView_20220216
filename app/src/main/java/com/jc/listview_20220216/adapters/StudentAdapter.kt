13f2ee90721fc0e92f9a30f23e63522fe02edab8
JaeCheon <studyJcHub@gmail.com>
2022-02-16 15:36:29 +0900

    tempRow 를 검사해서, null 이라면  - 새로 xml 을 끌어와서 대입시키기

--- app/src/main/java/com/jc/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/jc/listview_20220216/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.jc.listview_20220216.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.jc.listview_20220216.R
 import com.jc.listview_20220216.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,18 @@ class StudentAdapter(
     val resId: Int,
     val mList: ArrayList<StudentData>
 ): ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        // Row 는 보통 한 칸을 의미한다.
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            // inflate -> 어떤 레이아웃을 사용해서 코틀린 파일에서 사용 할 것인지
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+        }
+
+
+    }
+
 }
